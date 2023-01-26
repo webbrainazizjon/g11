5b5705d15ba21a2708e7ea50303ecd3a9fcf1926
ziyoviddinovazizjon@gmail.com <ziyoviddinovazizjon@gmail.com>
2023-01-27 00:58:46 +0300

    1-dars

--- index.js
+++ index.js
@@ -4,5 +4,60 @@
 // console.log(`hello ${name}`);
 
 
-let name = 'hello'
-console.log(`hello ${neme}`);
+// let name = 'hello'
+// console.log(`hello ${name}`);
+
+// console.log('hi h1');
+// console.log('hi h2');
+// console.log('hi h3');
+
+// let name;   //declaration
+// console.log(name);
+// name = 1;  //assign
+
+// let num1 =1;
+// let num2 =1;
+// let num1 = num2 =1;
+
+// let num1 = 1;
+// let num2 = num1;
+// console.log(num1,num2);
+
+// const num1 =1
+// console.log(num1);
+// num1 =2
+// console.log(num1);
+
+// let num1 = 12345
+// let num2 = `123 ${12345}45`
+// let num3 = 12345
+
+// console.log(num2);
+
+
+// console.log(9007199254740992 + 1); folse
+// console.log(9007199254740992n + 1n); true
+
+// let name
+// name = 1
+// name = null
+// console.log(name);
+
+// let id1 = Symbol(1234)
+// let id2 = Symbol(1234)
+// console.log(id1===id2);
+
+// let name = 'Azizbek'
+// let surname = 'Ziyoviddinov'
+// let age = 18
+// let user = {name: 'Azizbek',surname:'Ziyoviddinov',age:18}
+// let univ = [user,user,user,user,user]
+// console.log(univ);
+
+// let name =1
+// let name1 =1
+// console.log(name===name1);
+
+// let obj1 = {name: 1}
+// let obj2 = {name:1}
+// console.log(obj1===obj2);
