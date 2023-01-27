2353d48e7e9669abca3420a0d11aacfd086e35c6
ziyoviddinovazizjon@gmail.com <ziyoviddinovazizjon@gmail.com>
2023-01-27 11:50:14 +0300

    2-dars

---
+++ 2-dars/index.js
@@ -0,0 +1,107 @@
+// let b = 1;
+// let a = '1';
+// console.log(b + +a);
+// console.log(a + +b);
+
+// console.log(a * b);
+// console.log(+a - b);
+
+// let a = "10"
+// let b = "10.3"
+
+
+// console.log(Number(b));
+// console.log(Number.parseInt(b));
+// console.log(Number.parseFloat(b));
+// console.log(parseInt(b));
+// console.log(parseFloat(b));
+
+// console.log(Number(a));
+// console.log(Number.parseInt(a));
+// console.log(Number.parseFloat(a));
+
+// console.log(false == 0);
+// console.log(true == 1);
+// console.log(false == null);
+// console.log(false == undefined);
+// console.log(undefined == null);
+
+// console.log(false == "1");
+// console.log(false == "a");
+
+// ======================
+
+// let a = 1;
+// let b = "10";
+
+// console.log(a + +b);
+
+// console.log(1+1*1/1);
+// console.log(1+(1*1)/1);
+
+// ============  --, ++.
+
+// let a = 10;
+// // a++
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+// console.log(a);
+// console.log(a++);
+// console.log(a++);
+// console.log(++a);
+
+// =====================
+
+// Obrivations
+
+// let a = 50;
+// a = a + 10; // 50+10=60
+// a += 10 // 60+10=70
+// console.log(a);
+
+// ==================
+// Comparision
+
+// let a = 10;
+// let b = 11; // number
+// let c = "11"; // string
+// =, ==, ===
+// console.log(a > b);
+// console.log(a < b);
+// console.log(c <= b);
+// console.log(c >= b);
+// console.log(c == b);
+// console.log(c === b);
+
+
+// let star1 = "A";
+// let star2 = "a"
+
+// console.log(star1 > star2);
+// console.log(star1 < star2);
+// console.log("star1" > "star2");
+
+// let a = 1;
+// let b =2;
+// let check = true
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a != b);
+// console.log(check);
+// console.log(!check);
+
+//===========================
+// Logical Operations
+// OR ||
+// let ceo = true
+// let admin = false
+// console.log(ceo === true ||admin === true);
+// console.log(true || true);
+// console.log(ceo || admin);
+
+//AND &&
+
+// let id = true
+// let face = true
+// console.log(id && face);
