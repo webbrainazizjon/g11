c2e1707ee6e6bd070eed1b2fac25439abaef63fd
ziyoviddinovazizjon@gmail.com <ziyoviddinovazizjon@gmail.com>
2023-01-27 12:17:58 +0300

    1-dars

--- 1-dars/index.js
+++ 1-dars/index.js
@@ -1,7 +1,7 @@
 // let name = "Ilya";
-// console.log(`hello ${1}`);
-// console.log(`hello ${"name"}`);
-// console.log(`hello ${name}`);
+// console.log(`hello ${1}`);  hello 1
+// console.log(`hello ${"name"}`); hello "name"
+// console.log(`hello ${name}`); hello Ilya
 
 
 // let name = 'hello'
---
+++ 1-dars/uyIshi.js
@@ -0,0 +1,56 @@
+// let name = "Ilya";
+// console.log(`hello ${1}`);// hello 1
+// console.log(`hello ${"name"}`);// hello name
+// console.log(`hello ${name}`); //hello Ilya
+
+//1.number
+// let a = 1;
+// console.log(typeof a);
+
+// 2. Bigint
+// console.log(9007199254740992 + 1); /false
+// console.log(9007199254740992n + 1n); // true
+
+// 3. Stringlar
+// let hello = "hello"
+// console.log("user 'hello'");
+// console.log('user "hello"');
+// console.log(`user ${hello}`);
+
+// 4. Boolean
+// let oc = 1;
+// let oco = 1;
+// console.log(oc==oco);
+
+// let oc = 1;
+// let oco = 2;
+// console.log(oc==oco);
+
+// 5. null
+// let num = 1;
+// num = null
+// console.log(num);
+
+// 6. undefined
+
+// let num;
+// console.log(num);
+
+// 7. Symbols
+// let id1 = Symbol(1234)
+// let id2 = Symbol(1234)
+// console.log(id1===id2);
+
+// 8. object
+// object // array //function
+// let obj = {name:"Azizbek",fulname:"Ziyoviddinov"}
+// let array =[obj,obj,obj]
+// console.log(array);
+// let a =1;
+// let b =1;
+// function name(num) {
+//  num = a + b
+    
+//  console.log(num);
+// }
+// name()
