e699371340ee3df0d0fbf39e9413beb25af746ee
ziyoviddinovazizjon@gmail.com <ziyoviddinovazizjon@gmail.com>
2023-02-01 11:38:49 +0300

    homeworc

---
+++ 3-dars/homeworc.js
@@ -0,0 +1,42 @@
+
+
+// if("0"){
+//     console.log("Hello");
+// }
+
+
+// let ad = 11;
+
+// if(ad > 10){
+//     console.log(1);
+// } else if(ad > 5){
+//     console.log(-1);
+// }else if(ad < 1){
+//     console.log(0);
+// }
+
+// let on = "EcmoScripti"
+
+// if(on === "EcmoScript"){
+//     console.log("EcmoScript");
+// }else{
+//     console.log("no EcmoScript");
+// }
+
+// let a = 2;
+// let b = 1;
+// if(a+b<4){
+//     console.log('Below');
+// } else{
+//     console.log('Over');
+// }
+
+// if(login == "Employe") console.log("Hello");
+// else if(login == "Director") console.log("Greetings");
+// else if(login == '') console.log("no login");
+// else console.log('');
+
+
+// login == "Employe"? console.log("Hello"): console.log("Greetings");
+// login == "Employe"? console.log("Hello"): console.log("Greetings");
+
