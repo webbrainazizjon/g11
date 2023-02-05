eb9f0d74eabd86686b77d8a9b1a6456509e30ffa
ziyoviddinovazizjon@gmail.com <ziyoviddinovazizjon@gmail.com>
2023-02-06 00:09:58 +0300

    while, do while, functions

---
+++ 5-dars/homeworc.js
@@ -0,0 +1,54 @@
+// for (let i = 0;i < 3;i++) {
+//     console.log(`number ${i}!`);
+// } 
+//// bunda 
+//// number 0!
+//// number 1!
+//// number 2! chiqadi
+
+// let age = 19
+// function chackAge(age) {
+//     if (age >18) {
+//         return true;
+//     }else{
+        
+//        return console.log('Did parents allow you?');
+//     }
+// }
+
+// chackAge()
+
+
+
+// let age = 19
+// function chackAge(age) {
+//     if (age >18) {
+//         return true;
+//     }else{
+        
+//        return console.log('Did parents allow you?');
+//     }
+// }
+
+// chackAge()
+
+
+
+// min(2,5) == 2
+// min(3,-1) == -1
+// min(1,1) == 1
+
+// pow(3, 2) = 3 * 3 = 9;
+// pow(3, 3) = 3 * 3 * 3 = 27;
+// pow(1, 100) = 1 * 1 * 1 = 1;
+
+// function ask(question, yes, no) {
+//     if (condition(question)) yes();
+//     else no();
+// }
+
+// ask(
+//     "Do you agree?",
+//     function() {console.log("You agreed.");},
+//     function() {console.log("You canceled the execution.");}
+// );
