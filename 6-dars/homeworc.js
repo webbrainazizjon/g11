7b7cf30515773340f15783969e480c97796a4448
ziyoviddinovazizjon@gmail.com <ziyoviddinovazizjon@gmail.com>
2023-02-08 00:24:15 +0300

    objects-1

---
+++ 6-dars/homeworc.js
@@ -0,0 +1,32 @@
+//=============== HOMEWORC
+//  let user = {
+//     name:"John",
+//     surname:"Smith",
+//  }
+//  user.name = "Pete"
+//  delete user.name
+// console.log(user);
+
+// let salaries = {
+//     John:100,
+//     Ann:160,
+//     Pete:130
+// }
+
+// console.log();// yigindini topish
+
+
+// let menu = {
+//     width:200,
+//     height:300,
+//     title:"My menu"
+// }
+// console.log(menu); // faqat raqamlarni 2 ga bo'lish
+
+
+// let menu = {
+//     width:400,
+//     height:600,
+//     title:"My menu"
+// }
+// console.log(menu); // qiymatni o'zgartirish
