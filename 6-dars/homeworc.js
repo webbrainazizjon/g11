c5a40af8cbc0f73341edbac9650e7e2b0fc2eeaf
ziyoviddinovazizjon@gmail.com <ziyoviddinovazizjon@gmail.com>
2023-02-08 15:37:57 +0300

    objects-1

--- 6-dars/homeworc.js
+++ 6-dars/homeworc.js
@@ -9,25 +9,45 @@
 
 
 // let salaries = {
-//     John:100,
-//     Ann:160,
-//     Pete:130
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// };
+// let res = 0;
+// for (key in salaries) {
+//     if ( salaries[key] !== String) {
+//         res = res + salaries[key];
+//     } else {
+//         console.log(404);
+//     }
 // }
+// console.log(res);// yigindini topish
 
-// console.log();// yigindini topish
 
 
 // let menu = {
 //     width:200,
 //     height:300,
 //     title:"My menu"
+// };
+// let res = 0;
+
+// for (key in menu) {
+//     let qiymat = menu[key] / 2;
+//     res = res + (NaN != qiymat)
+//     console.log(qiymat);
 // }
-// console.log(menu); // faqat raqamlarni 2 ga bo'lish
 
 
 // let menu = {
 //     width:400,
 //     height:600,
 //     title:"My menu"
+// };
+// let res = 0;
+
+// for (key in menu) {
+//     let qiymat = menu[key] * 2;
+//     res = res + (NaN != qiymat)
+//     console.log(qiymat);
 // }
-// console.log(menu); // qiymatni o'zgartirish
