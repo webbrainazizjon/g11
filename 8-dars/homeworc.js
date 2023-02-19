48099c91924837d6928e5c8ffa5609f34581dd6e
ziyoviddinovazizjon@gmail.com <ziyoviddinovazizjon@gmail.com>
2023-02-20 00:09:35 +0300

    numbers math

---
+++ 8-dars/homeworc.js
@@ -0,0 +1,12 @@
+
+function name(a,b) {
+    if (a<b) {
+        console.log(a, "kichik");
+    } else if(a===b){
+        console.log(a,b , "teng");
+    }else{
+        console.log(b, "kichik");
+    }
+}
+
+name(2,4)
