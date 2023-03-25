1f33f511a7ca91bfc71757a148e40f06b18d6330
ziyoviddinovazizjon@gmail.com <ziyoviddinovazizjon@gmail.com>
2023-03-25 14:22:42 +0300

    advanced array

---
+++ 11-dars/homeworc.js
@@ -0,0 +1,35 @@
+let users = [
+  { id: 1, year: 1998, engine:1, name: "Kobalt" },
+  { id: 2, year: 2005, engine:1.2, name: "Jentra" },
+  { id: 3, year: 2010, engine:1.6, name: "Spark" },
+  { id: 4, year: 2010, engine:1.5, name: "Lasetti" },
+  { id: 5, year: 2012, engine:2, name: "Kaptiva" },
+  { id: 6, year: 2000, engine:1.2, name: "Lada" },
+  { id: 7, year: 2018, engine:1.4, name: "Trekkr" },
+];
+
+
+// let res = users.filter((value , index)=>{
+//     return value.year <= 2000 
+// })
+// console.log(res);
+
+// let ress = users.filter((value , index)=>{
+//     return value.year > 2000 
+// })
+// console.log(ress);
+
+
+// let revi = users.sort((a,b)=>{ 
+//     return a.engine - b.engine})
+// console.log(revi);
+
+
+// let bich = users.sort((a,b)=>{
+//     return a.year - b.year})
+// console.log(bich);
+
+// let bic = users.sort((a,b)=>{ 
+//     return a.name.localeCompare(b.name)})
+//     console.log(bic);
+
